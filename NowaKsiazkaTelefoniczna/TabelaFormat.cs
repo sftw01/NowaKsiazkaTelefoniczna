bf09e659a83abc4769a3413e088b666bfd022fda
Konrad Mirek <sftwmail@gmail.com>
2024-05-14 00:50:14 +0200

    metoda WyswietlKontakt w klasie TabelaFormat do poprawy, chodzi o formatowanie tabeli i wyrwnanie kreseczek

--- NowaKsiazkaTelefoniczna/TabelaFormat.cs
+++ NowaKsiazkaTelefoniczna/TabelaFormat.cs
@@ -24,7 +24,7 @@ namespace NowaKsiazkaTelefoniczna
         {
             WyswietlNaglowek();
             WyswietlKontakt(ksiazka);
-            WyswietlLinie();
+            WyswietlLinie(KolorRamki);
             WyswietlIloscKontaktow(ksiazka);
         }
 
@@ -34,7 +34,7 @@ namespace NowaKsiazkaTelefoniczna
             WyswietlInfo(nazwaTabeli);
             WyswietlNaglowekKrotka();
             WyswietlKontakt(ksiazka);
-            WyswietlLinie();
+            WyswietlLinie(KolorRamki);
             WyswietlIloscKontaktow(ksiazka);
         }
         //wyswietl metode przeciazona WyswietlTabele o nazwe Tabeli i kolor nazwy
@@ -43,7 +43,7 @@ namespace NowaKsiazkaTelefoniczna
             WyswietlInfo(nazwaTabeli, kolorNazwy);
             WyswietlNaglowek();
             WyswietlKontakt(ksiazka);
-            WyswietlLinie();
+            WyswietlLinie(KolorRamki);
             WyswietlIloscKontaktow(ksiazka);
         }
 
@@ -53,7 +53,7 @@ namespace NowaKsiazkaTelefoniczna
             WyswietlInfo(nazwaTabeli, kolorNazwy);
             WyswietlNaglowekKrotka();
             WyswietlKontakt(ksiazka);
-            WyswietlLinie();
+            WyswietlLinie(KolorRamki);
 
         }
 
@@ -62,35 +62,37 @@ namespace NowaKsiazkaTelefoniczna
         //funkcja wyswietla ramke info z informacja, przyjmuje arg tekst
         public static void WyswietlInfo(string tekst)
         {
-            Funkcje.WyswietlTekst("-----------------------------------------------------------------------------------------------------------", KolorRamki);
+            WyswietlLinie(KolorRamki);
             Funkcje.WyswietlTekstBezNowejLinii("|", KolorRamki);
             Funkcje.WyswietlTekstBezNowejLinii(Funkcje.WysrodkujTekt(tekst, 105), KolorInfo);
             Funkcje.WyswietlTekst("|", KolorRamki);
-            Funkcje.WyswietlTekst("-----------------------------------------------------------------------------------------------------------", KolorRamki);
+            WyswietlLinie(KolorRamki);
         }
 
         //funkcja wyswietla ramke info z informacja, przyjmuje arg tekst i kolor
         public static void WyswietlInfo(string tekst, ConsoleColor kolor)
         {
-            Funkcje.WyswietlTekst("-----------------------------------------------------------------------------------------------------------", KolorRamki);
+            WyswietlLinie(KolorRamki);
             Funkcje.WyswietlTekstBezNowejLinii("|", KolorRamki);
             Funkcje.WyswietlTekstBezNowejLinii(Funkcje.WysrodkujTekt(tekst, 105), kolor);
             Funkcje.WyswietlTekst("|", KolorRamki);
-            Funkcje.WyswietlTekst("-----------------------------------------------------------------------------------------------------------", KolorRamki);
+            WyswietlLinie(KolorRamki);
         }
 
         //funkcja wyswietla ramke informacyjna o zawartosci pol
         public static void WyswietlNaglowek()
         {
-            Funkcje.WyswietlTekst("-----------------------------------------------------------------------------------------------------------", KolorRamki);
-            Funkcje.WyswietlTekst("|        Imie       |     Nazwisko      |  Numer telefonu  |     Adres     |  Ulubiony  |  Zablokowany    |", KolorRamki);
-            Funkcje.WyswietlTekst("-----------------------------------------------------------------------------------------------------------", KolorRamki);
+            WyswietlLinie(KolorRamki);
+            Funkcje.WyswietlTekst("|        Imie       |     Nazwisko      |  Numer telefonu  |     Adres     |  Ulubiony  |  Zablokowany    |      ID Token    |   ", KolorRamki);
+            WyswietlLinie(KolorRamki);
         }
         //funkcja wyswietla ramke informacyjna o zawartosci pol wrrsja skrocoa
         public static void WyswietlNaglowekKrotka()
         {
-            Funkcje.WyswietlTekst("|        Imie       |     Nazwisko      |  Numer telefonu  |     Adres     |  Ulubiony  |  Zablokowany    |", KolorRamki);
-            Funkcje.WyswietlTekst("-----------------------------------------------------------------------------------------------------------", KolorRamki);
+
+
+            Funkcje.WyswietlTekst("|        Imie       |     Nazwisko      |  Numer telefonu  |     Adres     |  Ulubiony  |  Zablokowany    |      ID Token    |   ", KolorRamki);
+            WyswietlLinie(KolorRamki);
         }
 
         //funkcja wyswietla informacje o kontaktach w tabeli
@@ -114,27 +116,29 @@ namespace NowaKsiazkaTelefoniczna
                 Funkcje.WyswietlTekstBezNowejLinii("| ", KolorRamki); Funkcje.WyswietlTekstBezNowejLinii(ulubiony.PadRight(11), ulubionyKolor);
                 Funkcje.WyswietlTekstBezNowejLinii("| ", KolorRamki); Funkcje.WyswietlTekstBezNowejLinii(zablokowany.PadRight(16), zablokowanyKolor);
 
+                Funkcje.WyswietlTekstBezNowejLinii("| ", KolorRamki); Funkcje.WyswietlTekstBezNowejLinii("  "+ kontakt.idToken.PadRight(10), kontakt.Kolor);
+
                 Funkcje.WyswietlTekst("| ", KolorRamki);
             }
 
         }
 
         //linia oddzielajaca
-        public static void WyswietlLinie()
+        public static void WyswietlLinie(ConsoleColor kolor)
         {
-            Funkcje.WyswietlTekst("-----------------------------------------------------------------------------------------------------------", KolorRamki);
+            Funkcje.WyswietlTekst("------------------------------------------------------------------------------------------------------------------------------", kolor);
         }
 
         //wyswiet ilosc kontaktow - ramka sformatowana ILOSC KONTAKTOW
         public static void WyswietlIloscKontaktow(Ksiazka ksiazka)
         {
             string ilosc = ksiazka.kontakty.Count.ToString();
-            Funkcje.WyswietlTekst("-----------------------------------------------------------------------------------------------------------", KolorRamki);
+            WyswietlLinie(KolorRamki);
             Funkcje.WyswietlTekstBezNowejLinii("|", KolorRamki);
             Funkcje.WyswietlTekstBezNowejLinii(Funkcje.WysrodkujTekt($"                                                                                 ILOSC KONTAKTOW: {ilosc}", 105), KolorInfo);
 
             Funkcje.WyswietlTekst("|", KolorRamki);
-            Funkcje.WyswietlTekst("-----------------------------------------------------------------------------------------------------------", KolorRamki);
+            WyswietlLinie(KolorRamki);
         }
 
 
